f373fe0ed6ac9c6b24224ccb917b4b3cfaba40c0
ANDI ARDIANSYAH NASIR <222111890@stis.ac.id>
2024-11-17 17:08:03 +0700

    create swiper image and clone-gpt

---
+++ clone-gpt/script.js
@@ -0,0 +1,7 @@
+const toggleThemeButton = document.querySelector("#theme-toggle-button");
+
+toggleThemeButton.addEventListener("click", () => {
+  const isLightMode = document.body.classList.toggle("light_mode");
+  localStorage.setItem("themeColor", isLightMode ? "light_mode" : "dark_mode");
+  toggleThemeButton.innerText = isLightMode ? "dark_mode" : "light_mode";
+});
